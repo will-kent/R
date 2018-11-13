361ec03bbba53775b3541bdc000a17b8e428d3eb
William Kent <will.kent@menulog.com>
2018-11-13 18:46:04 +1100

    add Text Mining Report and Titanic Survival code

---
+++ Machine Learning/Kaggle/Titanic/Titanic_Survival.R
@@ -0,0 +1,14 @@
+library(tidyverse)
+
+# Check working directory set correctly
+#getwd()
+#setwd("C:\\Users\\will.kent\\source\\repos\\R\\Machine Learning\\Kaggle\\Titanic")
+
+# Read csv's
+train <- read.csv(".\\Data\\train.csv")
+test <- read.csv(".\\Data\\test.csv")
+
+##################
+# Data Decisions #
+##################
+
