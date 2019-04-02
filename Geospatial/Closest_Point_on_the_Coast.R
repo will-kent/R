4b3bb95ced64e3959ebcb94ae97a0071330f62d2
Will Kent <wjokent@gmail.com>
2019-04-02 21:55:25 +1100

    fixed graphics and modified words

--- Geospatial/Closest_Point_on_the_Coast.R
+++ Geospatial/Closest_Point_on_the_Coast.R
@@ -13,6 +13,7 @@ wgs.84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
 # Use geocode_OSM (Open Street Maps) to get co-oridinates of Uluru
 loc <- geocode_OSM("Uluru", projection = wgs.84)
+
 longitude <- loc$coords["x"]
 latitude <- loc$coords["y"]
 
@@ -26,8 +27,8 @@ download.file("http://www.naturalearthdata.com/http//www.naturalearthdata.com/do
 unzip(zipfile = "coastlines.zip", 
       exdir = 'ne-coastlines-10m')
 
-coast <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
-
+coast <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp", p4s = wgs.84)
+coast
 # Turn Shape file to data.frame
 coast_df <- SpatialLinesDataFrame(coast,coast@data) 
 
@@ -55,9 +56,19 @@ closest_latitude <- dist_line[3]
 # Creates a nice chart along with finding the closest point on the coast
 th     <- seq(0,2*pi,len=1000)
 circle <- cbind(1.00001*min_degrees*cos(th)+loc_wkt$x,1.00001*min_degrees*sin(th)+loc_wkt$y)
-sp_circle <- SpatialLines(list(Lines(list(Line(circle)),ID="1")),proj4string=CRS(wgs.84))
+#sp_circle <- SpatialLines(list(Lines(list(Line(circle)),ID="1")),proj4string=CRS(wgs.84))
 
 # Plot on map
-plot(coast, asp=1)
-plot(loc_wkt, add = TRUE, col = "red", pch = 20)
-plot(sp_circle, add = TRUE, col = "blue", lty = 2)
+#plot(coast, asp=1)
+#plot(loc_wkt, add = TRUE, col = "red", pch = 20)
+#plot(sp_circle, add = TRUE, col = "blue", lty = 2)
+
+loc_df <- data.frame(loc_wkt)
+data.frame(circle)
+ggplot() +
+  geom_path(data = coast, aes(x = long, y = lat, group = group)) +
+  geom_path(data = data.frame(circle), aes(x = X1, y = X2), colour = "blue") + 
+  geom_point(data = data.frame(loc_wkt), aes(x = x, y = y), colour = "red") +
+  scale_x_continuous(limits = c(110, 155)) +
+  scale_y_continuous(limits = c(-45, -5)) +
+  coord_fixed()
