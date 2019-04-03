6871df2e33f68766ee1475b2a8178feb356d0698
will.kent <will.kent@menulog.com>
2019-04-03 20:54:39 +1100

    2nd draft for Uluru to sea

--- Geospatial/Closest_Point_on_the_Coast.R
+++ Geospatial/Closest_Point_on_the_Coast.R
@@ -1,3 +1,4 @@
+
 library(tmaptools) # for geocode_OSM
 library(rgeos) # for readWKT
 #library(maptools)
@@ -6,6 +7,7 @@ library(rgdal) # for CRS
 #library(raster)
 library(ggplot2)
 library(geosphere)
+.libPaths()
 
 # Set up projections - IMPORTANT - confirm the regions for projections are correct 3577 covers all of Australia
 #epsg.3577 <- "+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs"
