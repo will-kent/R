9aad1ac7ff12eae2962e2c6eccc2c9c5ecf588be
will-kent <wjokent@gmail.com>
2019-07-24 21:54:36 +1000

    cleaned hops code

--- Geospatial/Kangaroo_Hops_to_the_Coast.R
+++ Geospatial/Kangaroo_Hops_to_the_Coast.R
@@ -9,16 +9,21 @@ library(geosphere)
 library(elevatr)
 library(rgbif)
 library(sp)
-library(rlist)
 
-# Set up projections - IMPORTANT - confirm the regions for projections are correct 3577 covers all of Australia
-#epsg.3577 <- "+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=km +no_defs"
-#wgs.84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-wgs.84 <- get_proj4("WGS84", output = "character")
+# Define parameters
+# 1. Location of interest, 2. Number of points to sample
 location <- "Uluru"
+pts_2_sample <- 10000
+
+# Set up projections - IMPORTANT - confirm the regions for projections are correct
+# We'll stick with WGS84 projections for this project
+wgs.84 <- get_proj4("WGS84", output = "character")
+
+# Create file name to save location information to to reduce number of calls to the web
 loc_file <- paste0(location,".Rds")
 
-# Use geocode_OSM (Open Street Maps) to get co-oridinates of Uluru
+# If location information previously received use existing info else use geocode_OSM
+# (Open Street Maps) to get co-oridinates of location of interest and save to file
 if (file.exists(loc_file)) {
   loc_df <- readRDS(loc_file)
   } else {
@@ -27,34 +32,32 @@ if (file.exists(loc_file)) {
   saveRDS(loc_df, file = loc_file)
   }
 
+# Get latitude and longitude of place of interest and create Well Known Text
 longitude <- loc_df["x",]
 latitude <- loc_df["y",]
-
 loc_txt <- paste("POINT(",longitude," ",latitude,")")
 loc_wkt <- readWKT(loc_txt, p4s = CRS(wgs.84))
 
-df <- elevatr::get_elev_point(loc_wkt, src = "aws")
-df
+# Check elevation of location of interest - does it look right?
+elevatr::get_elev_point(loc_wkt, src = "aws")
 
-# Download coastlines file from natural earth data and unzip
+# If it hasn't already been downloaded get coastlines file from natural earth data
 coastline_file <- "coastlines.zip"
 if (!file.exists(coastline_file)) {
   download.file("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/ne_10m_coastline.zip", 
               destfile = coastline_file)
   }
 
+# Unzip coastline data and load into a SpatialLinesDataFrame object
 unzip(zipfile = coastline_file, 
       exdir = 'ne-coastlines-10m')
-
 coast <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp", p4s = wgs.84)
-summary(coast)
+
 # Turn Shape file to data.frame
 coast_df <- SpatialLinesDataFrame(coast,coast@data) 
 
-# gDistance uses WGS84 projection so units are in degrees
-min_degrees <- gDistance(loc_wkt,coast)
-min_degrees
-
+# Find the minimum distance to the coast in metres, as well as the point on the coast closest
+# to the location of interest
 dist_line <- dist2Line(loc_wkt, coast)
 closest_longitude <- dist_line[2]
 closest_latitude <- dist_line[3]
@@ -66,28 +69,21 @@ xy <- cbind(x,y)
 
 sp_line <- SpatialLines(list(Lines(Line(xy), ID = "a")))
 
-spdf <- spsample(x = sp_line, n = 100, type = "regular") 
-as.data.frame(spdf)
-plot(spdf)
-#Download Digital Elevation Model (DEM) from Geoscience Australia - this is 25 metre grid
-#download.file("http://pid.geoscience.gov.au/dataset/ga/89676/export-full-1560937645581.zip",
-#              destfile = "Australia_25_")
-#?extract
-#extract(sp_line)
+# Break the line into n regular intervals and create a dataframe of these, adding the location
+# of the point of interest along with the point on the coast
+spdf <- spsample(x = sp_line, n = pts_2_sample, type = "regular") 
+points <- as.data.frame(spdf)
+points <- as.data.frame(rbind(xy[1,], points, xy[2,]))
 
-dist_degrees <- gDistance(loc_wkt,coast)
-
-th <- seq(0, 2 * pi, len = 1000)
-circle <- cbind(1.00001 * dist_degrees * cos(th) + loc_wkt$x,
-                1.00001 * dist_degrees * sin(th) + loc_wkt$y)
-
-# Plot on map
-a <- ggplot() +
+# Plot on map with line from location of interest to the coast
+ggplot() +
   geom_path(data = coast, aes(x = long, y = lat, group = group)) +
-  geom_path(data = data.frame(circle), aes(x = X1, y = X2), colour = "blue")+
   geom_point(data = data.frame(loc_wkt), aes(x = x, y = y), colour = "red") +
+  geom_path(data = points, aes(x = x, y = y), colour = "green") +
   scale_x_continuous(limits = c(110, 155)) +
   scale_y_continuous(limits = c(-45, -5)) +
   coord_fixed()
 
-a + plot(spdf)
+# Now get elevation of points, a SpatialPointsDataFrame is returned
+elevations <- elevatr::get_elev_point(points, prj = wgs.84 ,src = "aws")
+elevations
