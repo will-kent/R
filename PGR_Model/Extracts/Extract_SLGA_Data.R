1e7c455f53afe548f64182cf24ad057a78c84ab9
will.kent <will.kent@menulog.com>
2019-08-12 19:51:12 +1000

    function to get median SLGA data for a paddock

--- PGR_Model/Extracts/Extract_SLGA_Data.R
+++ PGR_Model/Extracts/Extract_SLGA_Data.R
@@ -1,46 +1,59 @@
 library(slga)
 library(raster)
 library(rgdal)
+library(readr)
 #require(slga)
 
-urlRoot <- "http://www.asris.csiro.au/arcgis/services"
-ausData <- "ACLEP_AU_NAT_C"
 
-productInfo <- "CLY"
-## Depth to Rock DER - Metres
-## Rooting Depth DPE - Metres
-## Organic Carbon SOC - %
-## pH Soil Water PHW
-## pH Soil CaCl2 PHC
-## Clay CLY - %
-## Silt SLT - %
-## Sand SND - %
-## Available Water Capactiy AWC - %
-## Total P PTO - %
-## Total N NTO - %
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+#getwd()
 
-depthInfo <- 1
-## 1: 0-5cm
-## 2: 5-15cm
-## 3: 15-30cm
-## 4: 30-60cm
-## 5: 60-100cm
-## 6: 100-200cm
+getPaddockMedianValues <- function(min_long, min_lat, max_long, max_lat, output_path, file_prefix) {
+  df_data <- data.frame(prod = character(),
+                        val = double())
+  output_csv <- paste0(output_path,"/",file_prefix,".csv")
+  
+  products <- list("CLY","SLT","SND","SOC","PTO","NTO","AWC")
+  ## Depth to Rock DER - Metres
+  ## Rooting Depth DPE - Metres
+  ## Organic Carbon SOC - %
+  ## pH Soil Water PHW
+  ## pH Soil CaCl2 PHC
+  ## Clay CLY - %
+  ## Silt SLT - %
+  ## Sand SND - %
+  ## Available Water Capactiy AWC - %
+  ## Total P PTO - %
+  ## Total N NTO - %
 
-aoi <- c(152.95, -27.55, 153.07, -27.45)
-soil_url <- slga:::make_soils_url(product = 'NAT', attribute = 'CLY', component = 'VAL', depth = 1, aoi = aoi)
-surface_clay <- slga:::get_slga_data(url = soil_url, out_temp = "C://temp//a.tif")
+  depthInfo <- 1
+  ## 1: 0-5cm
+  ## 2: 5-15cm
+  ## 3: 15-30cm
+  ## 4: 30-60cm
+  ## 5: 60-100cm
+  ## 6: 100-200cm
 
-clay_content <- raster("C://temp//a.tif")
+  aoi <- c(min_long, min_lat, max_long, max_lat)
+  
+  for (product in products) {
+    output_tif <- paste0(output_path,"/Soil.tif") 
+    soil_url <- slga:::make_soils_url(product = 'NAT', attribute = product, component = 'VAL', depth = 1, aoi = aoi)
+    
+    if (file.exists(output_tif)){
+      file.remove(output_tif)
+    }
+    
+    download_data <- slga:::get_slga_data(url = soil_url, out_temp = output_tif)
+    raster_data <- raster(output_tif)
+    sgdf_data <- as(raster_data, "SpatialGridDataFrame")
+    val <- median(sgdf_data@data$Soil)
+    df <- data.frame(cbind(product, val))
+    df_data <- rbind(df_data, df)
+  }
+  write_csv(df_data, output_csv)
 
-df_clay_content <- as(clay_content, "SpatialGridDataFrame")
-df_clay_content@data[df_clay_content@data < 0] <- 0
-plot(df_clay_content)
+}
 
-new_df <- cbind(coordinates(df_clay_content), df_clay_content@data)
-names(new_df)[1] <- "longitude"
-names(new_df)[2] <- "latitude"
-names(new_df)[3] <- "clay_content"
-new_df
-
+getSoilDetails(152.691, -30.99, 152.693, -30.98, getwd(),"Dads-Paddock")
 
